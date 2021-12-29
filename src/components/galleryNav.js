293724add355287beab50b035693e3f0533f2249
gonzalo-s <gonzalosoler@gmail.com>
2021-12-29 17:15:33 -0300

    deleted unnecesary files from project

--- src/components/galleryNav.js
+++
@@ -1,9 +0,0 @@
-import { Box } from "@chakra-ui/react";
-
-export default function GalleryNav() {
-  return (
-    <Box w="100%" border="1px" display="flex" justifyContent="center">
-      nav
-    </Box>
-  );
-}
